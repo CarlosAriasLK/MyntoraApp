1de6d629cd327011a555a07ffbff8f89e7bb5422
Carlos Arias <ceau922@gmail.com>
2025-09-16 23:04:47 -0500

    Modo oscuro

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -11,30 +11,32 @@ class AppTheme {
   final bool isDarkMode;
   AppTheme({required this.isDarkMode});
 
-
   ThemeData getTheme() => ThemeData(
     colorSchemeSeed: colorSeed,
     brightness: isDarkMode ? Brightness.dark : Brightness.light,
 
-    textTheme: TextTheme(
-      titleLarge: GoogleFonts.inter()
-        .copyWith( fontSize: 40, fontWeight: FontWeight.bold ),
-      titleMedium: GoogleFonts.inter()
-        .copyWith( fontSize: 30, fontWeight: FontWeight.bold ),
-      titleSmall: GoogleFonts.inter()
-        .copyWith( fontSize: 20 ),
-    ),
-
-    scaffoldBackgroundColor: scaffoldBackgroundColor,
+    scaffoldBackgroundColor: isDarkMode ? Colors.black : Colors.white,
 
     drawerTheme: DrawerThemeData(
-      backgroundColor: scaffoldBackgroundColor
+      backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
     ),
 
     appBarTheme: AppBarTheme(
-      color: scaffoldBackgroundColor,
-      titleTextStyle: GoogleFonts.inter()
-        .copyWith( fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black ),
+      color: isDarkMode ? Colors.black : Colors.white,
+      titleTextStyle: GoogleFonts.inter().copyWith(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        color: isDarkMode ? Colors.white : Colors.black,
+      ),
+    ),
+
+    textTheme: TextTheme(
+      titleLarge: GoogleFonts.inter()
+          .copyWith( fontSize: 40, fontWeight: FontWeight.bold ),
+      titleMedium: GoogleFonts.inter()
+          .copyWith( fontSize: 30, fontWeight: FontWeight.bold ),
+      titleSmall: GoogleFonts.inter()
+          .copyWith( fontSize: 20 ),
     ),
 
     snackBarTheme: SnackBarThemeData(
